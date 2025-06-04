37ca27305d058a7927140134c73fdb27db98d255
LavanyaPola <polalavanya51@gmail.com>
2025-06-04 11:07:22 +0530

    Add files via upload

---
+++ EMS_SQL.sql
@@ -0,0 +1,8 @@
+create database emp;
+use emp;
+
+select * from admin;
+select * from employees_table;
+
+insert into  admin (email, password) values('polalavanya@gmail.com','lavanya');
+select email, password from admin;
